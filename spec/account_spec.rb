7b0420aa3abff86bf946719364cee9c350c5e872
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-09-07 11:10:12 +0100

    Added withdraw method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,8 +4,19 @@ describe Account do
   describe '#deposit' do
     it 'deposits 1000' do
       account = Account.new
+      expect(account.balance).to eq(0)
       account.deposit(1000)
       expect(account.balance).to eq(1000)
+    end
+  end
+
+  describe '#withdrawal' do
+    it 'deposits and makes a withdrawal' do
+      account = Account.new
+      account.deposit(1000)
+      expect(account.balance).to eq(1000)
+      account.withdraw(500)
+      expect(account.balance).to eq(500)
     end
   end
 end
